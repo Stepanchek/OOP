8ebaa16118ee328469c2e30d95a0791843fe5fa8
Stepanchek <stepan_ilchuk@email.ua>
2022-12-07 22:39:26 +0200

    task6: Michael Scott queue

---
+++ Tasks/task6/src/main/java/org/example/Node.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+class Node<T> {
+    T data = null;
+    AtomicReference<Node> next = new AtomicReference<>();
+    Node(T data) {
+        this.data = data;
+    }
+    Node() { }
+}
